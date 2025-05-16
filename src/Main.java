95ecd752e2cc113796525fd951ae4841f02a7a12
PC-User <prue02@office.cju.ac.kr>
2025-05-16 13:41:30 +0900

    -

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        for (int n = 1; n <= 100; n++) {
+            System.out.println("1부터 100사이의 숫자를 입력하세요.");
+            num = sc.nextInt();
+            if (1 <= num && num <= 100) {
+                System.out.println("입력 완료! 프로그램을 종료합니다.");
+                break;
+            }
         }
     }
 }
